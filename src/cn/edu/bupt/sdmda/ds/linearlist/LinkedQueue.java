b247fa39accd79b342008af22f1ca9a7db376835
liuyang <liuyang@bupt.cn>
2018-10-30 16:16:46 +0800

    add LinkedQueue

---
+++ src/cn/edu/bupt/sdmda/ds/linearlist/LinkedQueue.java
@@ -0,0 +1,26 @@
+package cn.edu.bupt.sdmda.ds.linearlist;
+
+public class LinkedQueue<T> extends MyLinkedList<T> implements MyQueue<T> {
+	public LinkedQueue() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	@Override
+	public void offer(T t) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public T poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getHead() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/cn/edu/bupt/sdmda/main/DSMain.java
+++ src/cn/edu/bupt/sdmda/main/DSMain.java
@@ -1,6 +1,8 @@
 package cn.edu.bupt.sdmda.main;
 
+
 import cn.edu.bupt.sdmda.ds.linearlist.LinearList;
+import cn.edu.bupt.sdmda.ds.linearlist.LinkedQueue;
 import cn.edu.bupt.sdmda.ds.linearlist.LinkedStack;
 import cn.edu.bupt.sdmda.ds.linearlist.MyLinkedList;
 import cn.edu.bupt.sdmda.ds.linearlist.SeqList;
@@ -13,6 +15,7 @@ import cn.edu.bupt.sdmda.main.NumBaseConvertor.Base;
 
 public class DSMain {
 	public static void main(String[] args) {
+				
 		if(args.length>=2){
 			switch(args[0]){
 			case "linearlist":
@@ -35,35 +38,34 @@ public class DSMain {
 				break;
 			}
 		}
-
+		
 	}
-
+	
 	public static void testLinearListMain(String[] args) {
-
 		SeqList<Integer> seql = new SeqList<Integer>(args.length-1, 0);
 		for(int i=0; i<args.length-1; ++i){
 			seql.set(i, Integer.parseInt(args[i+1]));
 		}
 		testLinearList(seql);
-
+				
 		System.out.println("=======");
-
-		MyLinkedList<Integer> lnkl = new MyLinkedList<Integer>(args.length - 1, 0);
+		
+		MyLinkedList<Integer> lnkl = new MyLinkedList<Integer>(args.length, 0);
 		for(int i=0; i<args.length-1; ++i){
 			lnkl.set(i, Integer.parseInt(args[i+1]));
 		}
 		testLinearList(lnkl);
 	}
-
-
+	
+	
 	private static void testLinearList(LinearList<Integer> ll){
 		Integer ele1 = ll.get(0)+ll.get(1);
 		Integer ele2 = ll.get(0)-ll.get(1);
 		Integer ele3 = ll.get(0)*ll.get(1);
-
+		
 		// print size and all
 		printInfoOfLinearList(ll);
-
+		
 		// insert in head
 		// insert in mid
 		// insert in tail
@@ -71,10 +73,10 @@ public class DSMain {
 		ll.insert(0, ele1);
 		ll.insert(ll.getSize()/2, ele2);
 		ll.insert(ll.getSize(), ele3);
-
+		
 		// print size and all
 		printInfoOfLinearList(ll);
-
+		
 		// delete in head
 		// delete in mid
 		// delete in tail
@@ -83,28 +85,28 @@ public class DSMain {
 		System.out.print(ll.deleteAt(0)+"\t");
 		System.out.print(ll.deleteAt(ll.getSize()/2)+"\t");
 		System.out.println(ll.deleteAt(ll.getSize()-1));
-
+		
 		// print size and all
 		printInfoOfLinearList(ll);
-
+		
 		// find element like the tail
 		// delete element like the tail
 		System.out.println("finding last element");
 		Integer found = ll.find(ll.get(ll.getSize()-1));
 		System.out.println(found);
 		ll.delete(ll.get(found));
-
+		
 		// print size and all
 		printInfoOfLinearList(ll);
-
+		
 		// clear
 		System.out.println("clearing");
 		ll.clear();
 		// print size and all
 		printInfoOfLinearList(ll);
 	}
-
-
+	
+	
 
 	private static void printInfoOfLinearList(LinearList<Integer> ll){
 		System.out.println("List Summay:");
@@ -114,12 +116,12 @@ public class DSMain {
 		}
 		System.out.println("");
 	}
-
+	
 	public static void testStackMain(String[] args){
-
+		
 		SeqStack<String> s = new SeqStack<String>();
 		System.out.println("Push and pop half of input");
-		// push
+		// push 
 		int i=1;
 		for(;i<args.length/2;++i){
 			s.push(args[i]);
@@ -135,13 +137,13 @@ public class DSMain {
 		while(!s.isEmpty()){
 			System.out.println(s.pop());
 		}
-
-
-
-
+		
+		
+		
+		
 		LinkedStack<String> ls = new LinkedStack<>();
 		System.out.println("Push and pop half of input");
-		// push
+		// push 
 		i=1;
 		for(;i<args.length/2;++i){
 			ls.push(args[i]);
@@ -157,35 +159,55 @@ public class DSMain {
 		while(!ls.isEmpty()){
 			System.out.println(ls.pop());
 		}
-
+		
 	}
-
+	
 	public static void testQueueMain(String[] args){
-		SeqQueue<String> q = new SeqQueue<String>();
+		SeqQueue<String> sq = new SeqQueue<String>();
 		System.out.println("Offer and poll half of input");
-		// offer
+		// offer 
 		int i=1;
 		for(;i<args.length/2;++i){
-			q.offer(args[i]);
+			sq.offer(args[i]);
 		}
 		// poll
-		while(!q.isEmpty()){
-			System.out.println(q.poll());
+		while(!sq.isEmpty()){
+			System.out.println(sq.poll());
 		}
 		System.out.println("===half===");
 		for(;i<args.length;++i){
-			q.offer(args[i]);
+			sq.offer(args[i]);
 		}
-		while(!q.isEmpty()){
-			System.out.println(q.poll());
+		while(!sq.isEmpty()){
+			System.out.println(sq.poll());
+		}
+		
+		
+		LinkedQueue<String> lq = new LinkedQueue<String>();
+		System.out.println("Offer and poll half of input");
+		// offer 
+		i=1;
+		for(;i<args.length/2;++i){
+			lq.offer(args[i]);
+		}
+		// poll
+		while(!lq.isEmpty()){
+			System.out.println(lq.poll());
+		}
+		System.out.println("===half===");
+		for(;i<args.length;++i){
+			lq.offer(args[i]);
+		}
+		while(!lq.isEmpty()){
+			System.out.println(lq.poll());
 		}
 	}
-
+	
 	public static void calculator(String exp){
 		Calculator c = new Calculator(exp);
 		System.out.println(c.calc());
 	}
-
+	
 	public static void convert(String src, String sBase, String dBase){
 		System.out.println( NumBaseConvertor.Convert(
 				src, 
@@ -223,8 +245,8 @@ public class DSMain {
 		tree.postOrder(tree.getRoot(), func);
 		System.out.println("=====");
 	}
-
-
+	
+	
 }
 
 
@@ -236,5 +258,5 @@ class PrintTraverse<T> implements TraverseFuction<T>{
 		System.out.println(node.getData());
 	}
 
-
+	
 }
